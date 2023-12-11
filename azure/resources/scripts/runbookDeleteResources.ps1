e064210f3a93b2b9a21929309296566bbf1ae23c
Sandeep Doebar <sandoebar@homail.com>
2023-12-11 17:41:07 +0100

    changes ft 2pac

--- azure/resources/scripts/runbookDeleteResources.ps1
+++
@@ -1,9 +0,0 @@
-
-# Ensures you do not inherit an AzContext in your runbook
-Disable-AzContextAutosave -Scope Process
-
-# Connect to Azure with system-assigned managed identity
-Connect-AzAccount -Identity -AccountId a92ded5f-6277-4c95-b681-3221083f945a
-
-# set and store context
-Get-AzResourceGroup -Name teknologi-eur1-main-tst-rg  | Remove-AzResourceGroup -Force
